ba75a20106932fb8eb85b2a6e5fff63569e65332
k2m13 <kamil.sterniczuk@ou.ac.uk>
2026-05-31 23:00:43 +0100

    Retrieve target screen identifier

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,8 @@ console.log(screens);
 navLinks.forEach(function (link) {
     link.addEventListener("click", function () {
         console.log(link.textContent);
+    const screenId = link.getAttribute("href");
+        console.log(screenId);
     });
 });
+
