04e2e85c78b414e23f2225963613deb4876d7a5d
k2m13 <kamil.sterniczuk@ou.ac.uk>
2026-05-31 23:04:24 +0100

    Prevent default navigation behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,18 @@ const screens = document.querySelectorAll(".screen");
 console.log(navLinks);
 console.log(screens);
 
+// Log nav links and screen id's to the console
 navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-        console.log(link.textContent);
+  link.addEventListener("click", function () {
+    console.log(link.textContent);
     const screenId = link.getAttribute("href");
-        console.log(screenId);
-    });
+    console.log(screenId);
+  });
 });
 
+// Stop the page jumping
+link.addEventListener("click", function (event) {
+  event.preventDefault();
+  const screenId = link.getAttribute("href");
+  console.log(screenId);
+});
