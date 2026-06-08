607d4eb1fb6deefb3c44081efd0c4d6ed8674cbf
k2m13 <kamil.sterniczuk@ou.ac.uk>
2026-06-08 23:54:08 +0100

    Expand Jest coverage for winner logic

--- _tests_/gameLogic.test.js
+++ _tests_/gameLogic.test.js
@@ -24,3 +24,23 @@ test("paper beats spock", () => {
 test("computer wins when player chooses rock and computer chooses paper", () => {
   expect(determineWinner("rock", "paper")).toBe("computer");
 });
+
+test("scissors beats lizard", () => {
+  expect(determineWinner("scissors", "lizard")).toBe("player");
+});
+
+test("lizard beats paper", () => {
+  expect(determineWinner("lizard", "paper")).toBe("player");
+});
+
+test("spock beats rock", () => {
+  expect(determineWinner("spock", "rock")).toBe("player");
+});
+
+test("computer wins when scissors beats paper", () => {
+  expect(determineWinner("paper", "scissors")).toBe("computer");
+});
+
+test("computer wins when lizard beats spock", () => {
+  expect(determineWinner("spock", "lizard")).toBe("computer");
+});
